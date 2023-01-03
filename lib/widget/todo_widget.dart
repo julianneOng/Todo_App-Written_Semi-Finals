577590c1b9b1086656adb77922a00d4ba0ec0d27
julianneOng <101238387+julianneOng@users.noreply.github.com>
2023-01-03 17:06:28 +0800

    Update todo_widget.dart
    Update Commit

--- lib/widget/todo_widget.dart
+++ lib/widget/todo_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:todo_app_ui_ii_example/model/todo.dart';
 import 'package:todo_app_ui_ii_example/page/edit_todo_page.dart';
 import 'package:todo_app_ui_ii_example/provider/todos.dart';
